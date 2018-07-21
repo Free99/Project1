00fa5013ace3e85ab894d17ef32b2e3a745548d0
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-07-21 11:15:43 -0500

    I was able to accesss the database and pull all the info from each listing node. I was able to have a simple check to see if the zipcode entered in the search by the user actually exists in the databse

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,126 +1,60 @@
 //this is a test, to make sure im in the right location
 //hopefully this shit is working
 // Initialize Firebase
-var config = {
+
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyB0pMtOIhxz6BT26CaqP1xW2m6rVxMTFAo",
     authDomain: "group-project-d9732.firebaseapp.com",
     databaseURL: "https://group-project-d9732.firebaseio.com",
     projectId: "group-project-d9732",
-    storageBucket: "",
+    storageBucket: "group-project-d9732.appspot.com",
     messagingSenderId: "252744545653"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//this function will take the zipcode entered into the search bar and order the database based on that input
-function orderDatabase(zipcode){
 
 
-
-}
-
+//CODE COMMENT OUT START
 
 //create function to fill database with information based on zipcode provided
-
-function enterData(n,str,c,st,z,ft,fd,fw,fc) {
-
-    var count = 0;
-
-        //check database to see if zip code user entered is already a node within the database
-    database.ref().forEach(function(data){
-        if(data.key == z){
-            count++;
-        }
-    });
-
-    //if zipcode does not exist within database, create new node for that zipcode 
-    if(count === 0){
+//filling the database is on hold
 
 
 
-    //else zipcode exists and we can go to that node and save data there
-    } else{
+// //event listener for submit button
+// $("#submitListing").on("click",function(){
 
-        // var newListingKey = database.ref().child(z).push().key;
+//     event.preventDefault();
 
-        var listingData = {
+//     //can have some sort of validation, add this later
+//     //still waiting on IDs for data entered by user
+//     var tempName = $("#inputName").val().trim();
+//     var tempStreet = $("#inputAddress").val().trim();
+//     var tempCity = $("#inputCity").val().trim();
+//     var tempState = $("#inputState").val().trim();
+//     var tempZip = $("#inputZip").val().trim();
+//     var tempFurnType = $("#inputFurnType").val().trim();
+//     var tempFurnDims = $("#inputFurnDim").val().trim();
+//     var tempFurnWeight = $("#ipnutFurnWeight").val().trim();
 
-            name: n,
-            street: str,
-            city: c,
-            state: st,
-            zipcode: z,
-            furnType: ft,
-            furnDim: fd,
-            furnWeight: fw,
-            furnCond: fc,
+//     //since the condition will be a button, need to tweak this to get the attribute associated with the button
+//     var tempFurnCond = $("#inputFurnCond").val();
+//     // var tempPic = $("#furnPicLinkid").val();
 
-        }
-        // Write the new listing data to the node with the existing zipcode.
-        var updates = {};
-        updates['/' + z + '/' + newListingKey] = listingData;
-        return database.ref().update(updates);
-
-
-    }
-
-
-
-}
-
-//event listener for submit button
-$("#submitListing").on("click",function(){
-
-    event.preventDefault();
-
-    //can have some sort of validation, add this later
-    //still waiting on IDs for data entered by user
-    var tempName = $("#inputName").val().trim();
-    var tempStreet = $("#inputAddress").val().trim();
-    var tempCity = $("#inputCity").val().trim();
-    var tempState = $("#inputState").val().trim();
-    var tempZip = $("#inputZip").val().trim();
-    var tempFurnType = $("#inputFurnType").val().trim();
-    var tempFurnDims = $("#inputFurnDim").val().trim();
-    var tempFurnWeight = $("#ipnutFurnWeight").val().trim();
-
-    //since the condition will be a button, need to tweak this to get the attribute associated with the button
-    var tempFurnCond = $("#inputFurnCond").val();
-    // var tempPic = $("#furnPicLinkid").val();
-
-    enterData(tempName,tempStreet,tempCity,tempcity,tempState,tempZip,tempFurnType,tempFurnDims,tempFurnWeight,tempFurnCond);
+//     enterData(tempName,tempStreet,tempCity,tempcity,tempState,tempZip,tempFurnType,tempFurnDims,tempFurnWeight,tempFurnCond);
         
         
 
 
-    });
+//     });
 
+//COMMENT OUT END
 
     
 
-    // database.ref().push({
-
-    //     name: tempName,
-    //     street: tempStreet,
-    //     city: tempCity,
-    //     state: tempState,
-    //     zip: tempZip,
-    //     furniture: {
-            
-    //         type: tempFurnType,
-    //         dims: tempFurnDims,
-    //         weight: tempFurnWeight,
-    //         cond: tempFurnCond,
-    //         picture: tempPic,
-
-
-
-    //     }
-
-
-
-    // });
 
 
 
@@ -130,17 +64,48 @@ $("#submitListing").on("click",function(){
 //before doing this need to research if we can order the database using the zipcode searched
 //if so i need to create a function that orders the database that is called everytime a user adds a listing or the page is refreshed
 //this kills two birds with one stone
-database.ref().on("value",function(snapshot){
-
-    //this function will order the database based on
-    orderDatabase();
 
 
 
+var ref =  database.ref("78745");
+
+
+ref.once("value").then(function(snapshot) {
+
+    var doesExsist = snapshot.exists();
+
+    //runs code if node exists within database
+    if(doesExsist){
+    console.log(snapshot.val());
+    //cycle through each listing within the database and write to the page simultaneously
+    snapshot.forEach(function(childSnap){
+
+        var tempName = childSnap.val().name;
+        console.log(tempName);
+        var tempZip = childSnap.val().zipcode;
+        console.log(tempZip);
+        var tempFurnType = childSnap.val().furnType;
+        console.log(tempFurnType);
+
+        //once we have the data here, It think it would be best to store the details within the divs
+        //we are creaeting for each listing, that way when the user clicks the div we do not have to
+        //access the database again
 
 
 
-}, function(errorObject){
 
-    console.log("The load failed" + errorObject.node);
-})
+    });
+
+
+
+
+    //if the data does not exsist, return "no listing in the area requested"
+    } else{
+
+        var noListingMessage = "sorry there are no listings in the area you requested"
+
+        console.log(noListingMessage);
+    }
+
+
+});
