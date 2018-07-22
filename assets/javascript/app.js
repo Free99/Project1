2322a8c7020a7ac97c37a956a2709c976fdbe0e6
LAPTOP-LBC3BOM6\juanc <juancamilo_11@utexas.edu>
2018-07-22 16:22:39 -0500

    Was able to structure database so users trying to listing furniture can create their listings and users searching for furniture can search using the zipcode search bar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,7 +17,7 @@ var database = firebase.database();
 
 
 
-//CODE COMMENT OUT START
+
 
 //create function to fill database with information based on zipcode provided
 //filling the database is on hold
@@ -25,33 +25,62 @@ var database = firebase.database();
 
 
 // //event listener for submit button
-// $("#submitListing").on("click",function(){
+ $("#submitListing").on("click",function(){
 
-//     event.preventDefault();
+     event.preventDefault();
 
-//     //can have some sort of validation, add this later
-//     //still waiting on IDs for data entered by user
-//     var tempName = $("#inputName").val().trim();
-//     var tempStreet = $("#inputAddress").val().trim();
-//     var tempCity = $("#inputCity").val().trim();
-//     var tempState = $("#inputState").val().trim();
-//     var tempZip = $("#inputZip").val().trim();
-//     var tempFurnType = $("#inputFurnType").val().trim();
-//     var tempFurnDims = $("#inputFurnDim").val().trim();
-//     var tempFurnWeight = $("#ipnutFurnWeight").val().trim();
+     //can have some sort of validation, add this later
+     //still waiting on IDs for data entered by user
+     var tempName = $("#inputName").val().trim();
+     var tempStreet = $("#inputAddress").val().trim();
+     var tempCity = $("#inputCity").val().trim();
+     var tempState = $("#inputState").val().trim();
+     var tempZip = $("#inputZip").val().trim();
+     var tempFurnType = $("#inputFurnType").val().trim();
+     var tempFurnDims = $("#inputFurnDim").val().trim();
+     var tempFurnWeight = $("#inputFurnWeight").val().trim();
+     var tempListing = $("#inputListing").val().trim();
 
-//     //since the condition will be a button, need to tweak this to get the attribute associated with the button
-//     var tempFurnCond = $("#inputFurnCond").val();
-//     // var tempPic = $("#furnPicLinkid").val();
+     //since the condition will be a button, need to tweak this to get the attribute associated with the button
+     var tempFurnCond = $("#inputFurnCond").val();
+     // var tempPic = $("#furnPicLinkid").val();
 
-//     enterData(tempName,tempStreet,tempCity,tempcity,tempState,tempZip,tempFurnType,tempFurnDims,tempFurnWeight,tempFurnCond);
+    //write some sort of input validation checking to make sure that all fields have been filled out
+    //if data is validated is true then push data, else prompt user to fill out all fields
+    database.ref("/Listings").push({
+        name: tempName,
+        street: tempStreet,
+        city: tempCity,
+        state: tempState,
+        zipcode: tempZip,
+        listing: tempListing,
+        furniture: tempFurnType,
+        dimensions: tempFurnDims,
+        weight: tempFurnWeight,
+        condition: tempFurnCond,
+
+
+
+    });
+
+    $("#inputName").val('');
+    $("#inputAddress").val('');
+    $("#inputCity").val('');
+    $("#inputStreet").val('');
+    $("#inputState").val('');
+    $("#inputZip").val('');
+    $("#inputFurnType").val('');
+    $("#inputFurnDim").val('');
+    $("#inputFurnWeight").val('');
+    $("#inputListing").val('');
+
+
         
         
 
 
-//     });
+    });
 
-//COMMENT OUT END
 
     
 
@@ -66,13 +95,82 @@ var database = firebase.database();
 //this kills two birds with one stone
 
 
-database.ref().push({
+$("#searchZip").on("click",function(){
 
-    77433: listing0,
-    
+    //this will be the zip code we are searching the databse for
+    event.preventDefault();
+
+    $("#listingbox").empty();
+
+    var searchZip = $("#inputSearchZip").val();
+
+    console.log(searchZip);
+
+    var listings = database.ref().child('Listings');
+    var query = listings.orderByChild('zipcode').equalTo(searchZip);
 
 
-})
+    query.once("value").then(function(snapshot) {
+
+        var doesExsist = snapshot.exists();
+
+        //runs code if node exists within database
+        if(doesExsist){
+        console.log(snapshot.val());
+        //cycle through each listing within the database and write to the page simultaneously
+        snapshot.forEach(function(childSnap){
+
+            var tempName = childSnap.val().name;
+            console.log(tempName);
+            var tempListing = childSnap.val().listing;
+            console.log(tempListing);
+            var tempZip = childSnap.val().zipcode;
+            console.log(tempZip);
+            var tempFurnType = childSnap.val().furnType;
+            console.log(tempFurnType);
+            var tempCity = childSnap.val().city;
+            console.log(tempCity);
+            var tempState = childSnap.val().state;
+            console.log(tempState);
+            var tempStreet = childSnap.val().street;
+            console.log(tempStreet);
+            var tempFurnWeight = childSnap.val().furnWeight;
+            console.log(tempFurnWeight);
+            var tempFurnDim = childSnap.val().furnDim;
+            console.log(tempFurnDim);
+            var tempFurnCond = childSnap.val().furnCond;
+            console.log(tempFurnCond);
+
+
+            var newListing = $('<li class="listing list-group-item" zip-value="'+ tempZip +'" city-value="'+ tempCity +'" street-value="'+ tempStreet +'" name-value="'+ tempName +'" furnType-value="'+ tempFurnType +'" furnWeight-value="'+ tempFurnWeight +'" furnDim-value="'+ tempFurnDim +'" furnCond-value="'+ tempFurnCond +'"> Listing: '+ tempListing +' Furniture: ' + tempFurnType + '</li>');
+            //once we have the data here, It think it would be best to store the details within the divs
+            //we are creaeting for each listing, that way when the user clicks the div we do not have to
+            //access the database again
+
+            $("#listingbox").append(newListing);
+
+
+        });
+
+
+
+
+        //if the data does not exsist, return "no listing in the area requested"
+        } else{
+
+        var noListingMessage = "sorry there are no listings in the area you requested"
+
+        $('#listingbox').append('<li class=list-group-item>' + noListingMessage + '</li>');
+
+        console.log(noListingMessage);
+        }
+
+
+    });
+
+
+
+});
 
 
 
